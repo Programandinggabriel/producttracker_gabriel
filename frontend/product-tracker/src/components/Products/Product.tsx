93008ff47e6e73571981f1287a76cb743f3bd09c
Gabriel Santiago <103392402+Programandinggabriel@users.noreply.github.com>
2026-09-03 11:08:31 -0500

    Agrego logo de provider a card product y product detail

--- frontend/product-tracker/src/components/Products/Product.tsx
+++ frontend/product-tracker/src/components/Products/Product.tsx
@@ -1,15 +1,20 @@
 'use client'
 
-import { type ItemProduct } from "@/src/services/products"
+import { Provider, type ItemProduct } from "@/src/services/products"
 import Image from "next/image";
 
 type ItemProductProps = {
-    product: ItemProduct
+    product: ItemProduct;
+    provider: Provider;
 }
 
-export default function Product({product}: ItemProductProps){
+export default function Product({ product, provider }: ItemProductProps){
     const thubnailImage = product.thumbnail;
     const shimmerB64 = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjMwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSIjZThlOGU4Ii8+PC9zdmc+"
+
+    const getUrlLogoProvider = (urlLogo: String) => {
+        return `${process.env.NEXT_PUBLIC_API_URL}${urlLogo}`;
+    }
 
     return(
     <>
@@ -33,15 +38,27 @@ export default function Product({product}: ItemProductProps){
                 >
                     <h5 className="text-md text-heading font-semibold tracking-tight">{product.title}</h5>
                 </a>
-                <a 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={product.url} 
-                    className="inline-flex font-medium items-center text-fg-brand hover:underline"
-                >
-                    Ver producto
-                    <svg className="w-4 h-4 ms-2 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/></svg>
-                </a>
+                <div className="flex flex-row">
+                    <a 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={product.url} 
+                        className="inline-flex font-medium items-center text-fg-brand hover:underline"
+                    >
+                        Ver producto
+                        <svg className="w-4 h-4 ms-2 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/></svg>
+                    </a>
+                    <div className="inline-flex rounded-fill ml-auto w-9 h-9">
+                        <Image
+                            src={getUrlLogoProvider(provider.logo)}
+                            alt={`logo-${provider.id}`}
+                            width={50}
+                            height={50}
+                            style={{ width: 'auto', height: 'auto' }}
+                            unoptimized
+                        />
+                    </div>
+                </div>
                 <div className="flex items-center justify-between mt-6">
                     <span className="text-md font-extrabold text-heading">{`$ ${product.price} ${product.currency}`}</span>
                 </div>

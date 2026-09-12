6041ffee1899a5d8a69193af7268b9f41eb0eb0d
Gabriel Santiago <103392402+Programandinggabriel@users.noreply.github.com>
2026-09-11 20:39:04 -0500

    Dockerize app with docker compose

--- frontend/product-tracker/src/app/home/products/page.tsx
+++ frontend/product-tracker/src/app/home/products/page.tsx
@@ -1,9 +1,12 @@
 import Manager from '@/src/components/Products/Manager'
+import { Suspense } from 'react'
 
 export default function Products(){
     return (
         <>
-            <Manager/>
+            <Suspense>
+                <Manager/>
+            </Suspense>
         </>
     )
 }

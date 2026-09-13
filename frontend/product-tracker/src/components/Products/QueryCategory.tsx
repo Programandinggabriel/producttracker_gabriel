22f41834d68ae3c25f0270fe73dc54bc8a357306
Gabriel Santiago <103392402+Programandinggabriel@users.noreply.github.com>
2026-09-12 21:36:20 -0500

    Corrigo dropdown fallando en build

--- frontend/product-tracker/src/components/Products/QueryCategory.tsx
+++ frontend/product-tracker/src/components/Products/QueryCategory.tsx
@@ -10,6 +10,7 @@ type QueryCategoryProps = {
 }
 
 export default function QueryCategory({ onCategoryChange, onSendQueryText }: QueryCategoryProps){
+    const [isDropDown, setIsDropDown] = useState(false);
     const [categories, setCategories] = useState<Category[]>([]);
     const [query, setQuery] = useState('');
     const [isApiError, setIsApiError] = useState(false);
@@ -56,24 +57,34 @@ export default function QueryCategory({ onCategoryChange, onSendQueryText }: Que
     return (
         <>
         <form onSubmit={(e) => {handleSubmitForm(e)}} className="max-w-2xl mx-auto">
-            <div className="flex shadow-xs rounded-base -space-x-0.5">
-                <button id="dropdown-button" data-dropdown-toggle="dropdown" type="button" className="inline-flex items-center shrink-0 z-10 text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-s-base text-sm px-4 py-2.5 focus:outline-none">
+            <div className="flex relative shadow-xs rounded-base -space-x-0.5">
+                <button 
+                    id="dropdown-button" 
+                    type="button" 
+                    className="inline-flex items-center shrink-0 z-10 text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-s-base text-sm px-4 py-2.5 focus:outline-none"
+                    onClick={() => setIsDropDown(prev => !prev)}
+                >
                     <svg className="w-4 h-4 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.143 4H4.857A.857.857 0 0 0 4 4.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 10 9.143V4.857A.857.857 0 0 0 9.143 4Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 20 9.143V4.857A.857.857 0 0 0 19.143 4Zm-10 10H4.857a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286A.857.857 0 0 0 9.143 14Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286a.857.857 0 0 0-.857-.857Z"/></svg>
                         Categorias
                     <svg className="w-4 h-4 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/></svg>
                 </button>
-                <div id="dropdown" className="z-10 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44 h-48 overflow-y-auto">
-                    <ul className="p-2 text-sm text-body font-medium" aria-labelledby="dropdown-button">
-                        {   categories.map((category) => {
-                                return (
-                                    <li key={category.id} onClick={() => onCategoryChange(category.id)}>
-                                        <p className="block p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded-md">{category.slug}</p>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
+                {isDropDown && (
+                    <div 
+                        id="dropdown" 
+                        className="z-10 absolute top-full left-0 bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44 h-48 overflow-y-auto"
+                    >
+                        <ul className="p-2 text-sm text-body font-medium">
+                            {   categories.map((category) => {
+                                    return (
+                                        <li key={category.id} onClick={() => onCategoryChange(category.id)}>
+                                            <p className="block p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded-md">{category.slug}</p>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                )}
                 <input 
                     onChange={(e) => {handleQueryChange(e)}} 
                     type="search" 
